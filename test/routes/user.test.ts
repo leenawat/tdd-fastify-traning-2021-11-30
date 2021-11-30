740256d4a456feadec63f36e0dd3ded75941c343
Leenawat Papahom <leenawat.pa@gmail.com>
2021-11-30 14:57:02 +0700

    refactoring test

--- test/routes/user.test.ts
+++ test/routes/user.test.ts
@@ -2,6 +2,20 @@ import { build } from '../helper'
 import db from '../../src/config/database'
 const app = build()
 
+const validUser = {
+    'first_name': 'Leenawat',
+    'last_name': 'Papahom',
+    'username': 'leenawat',
+    'password': 'P4ssword'
+}
+
+const postUser = async (user = validUser) => {
+    return await app.inject({
+        url: '/api/users',
+        method: 'post',
+        payload: validUser
+    })
+}
 
 describe('user tests', () => {
 
@@ -10,47 +24,20 @@ describe('user tests', () => {
     })
 
     it('returns 200 ok when signup request is valid', async () => {
-        const res = await app.inject({
-            url: '/api/users',
-            method: 'post',
-            payload: {
-                'first_name': 'Leenawat',
-                'last_name': 'Papahom',
-                'username': 'leenawat',
-                'password': 'P4ssword'
-            }
-        })
+        const res = await postUser()
         expect(res.statusCode).toBe(201)
     })
 
     it('returns success message when signup request is valid', async () => {
-        const res = await app.inject({
-            url: '/api/users',
-            method: 'post',
-            payload: {
-                'first_name': 'Leenawat',
-                'last_name': 'Papahom',
-                'username': 'leenawat',
-                'password': 'P4ssword'
-            }
-        })
+        const res = await postUser()
         expect(JSON.parse(res.payload)).toEqual({ message: 'User created' })
     })
 
-
     it('save the user to database', async () => {
-        await app.inject({
-            url: '/api/users',
-            method: 'post',
-            payload: {
-                'first_name': 'Leenawat',
-                'last_name': 'Papahom',
-                'username': 'leenawat',
-                'password': 'P4ssword'
-            }
-        })
+        await postUser()
         const userList = await db('users').select()
         expect(userList.length).toBe(1)
     })
 
 })
+
