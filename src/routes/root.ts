bbbb512a0c69ff864673ae767a96c67f8aab2640
Leenawat Papahom <leenawat.pa@gmail.com>
2021-11-30 12:30:30 +0700

    ตั้งค่า  log level ใน .env

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,6 +2,21 @@ import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
+    // fatal, error, warn, info, debug, trace
+    /**
+     * fatal -> fatal
+     * error -> fatal, error, 
+     * warn  -> fatal, error, warn
+     * info  -> fatal, error, warn, info
+     * debug -> fatal, error, warn, info, debug 
+     * trace -> fatal, error, warn, info, debug, trace
+     */
+     fastify.log.fatal('1 `fatal`')
+     fastify.log.error('2 `error`')
+     fastify.log.warn('3 `warn`')
+     fastify.log.info('4 `info`')
+     fastify.log.debug('5 `debug`')
+     fastify.log.trace('6 `trace`')
     return { root: true }
   })
 }
