3e9d11d963fc89c1b1bdfb77c7e63c56f1413b75
Leenawat Papahom <leenawat.pa@gmail.com>
2021-12-01 10:50:38 +0700

    กำหนดชื่อไฟล์และโฟลเดอร์ที่เก็บไฟล์

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -3,12 +3,25 @@ import multer from 'fastify-multer'
 
 // const mime = require('mime-type')
 // import * as fse from 'fs-extra'
-// import * as path from 'path'
 // import * as fs from 'fs'
+import * as path from 'path'
+import {v4 as uuidv4} from 'uuid'
 
 const upload: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     const uploadPath = './upload'
-    const upload = multer({ dest: uploadPath })
+
+    const storage = multer.diskStorage({
+        destination: (req, file, cb)=>{
+            cb(null, uploadPath)
+        },
+        filename: (req, file, cb)=>{
+            const _ext = path.extname(file.originalname)
+            const filename = uuidv4() + _ext
+            cb(null, filename)
+        }
+    })
+
+    const upload = multer({ storage })
     fastify.post('/api/upload', {
         preHandler: upload.single('file')
     }, async function (request, reply) {
