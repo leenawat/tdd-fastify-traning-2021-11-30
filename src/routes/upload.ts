492366c575d4ea732d03cd5abc82debc5e1899ba
Leenawat Papahom <leenawat.pa@gmail.com>
2021-12-01 10:59:08 +0700

    filter upload file

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -5,23 +5,35 @@ import multer from 'fastify-multer'
 // import * as fse from 'fs-extra'
 // import * as fs from 'fs'
 import * as path from 'path'
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 const upload: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     const uploadPath = './upload'
 
     const storage = multer.diskStorage({
-        destination: (req, file, cb)=>{
+        destination: (req, file, cb) => {
             cb(null, uploadPath)
         },
-        filename: (req, file, cb)=>{
+        filename: (req, file, cb) => {
             const _ext = path.extname(file.originalname)
             const filename = uuidv4() + _ext
             cb(null, filename)
         }
     })
-
-    const upload = multer({ storage })
+    // const upload = multer({dest: uploadPath})
+    const upload = multer({
+        storage,
+        limits: {
+            fileSize: 5 * 1024 * 1024
+        },
+        fileFilter: (req, file, cb) => {
+            // กำหนด type
+            if (file.mimetype !== 'image/png') {
+                return cb(new Error('Invalid mimetype!'), false)
+            }
+            cb(null, true)
+        }
+    })
     fastify.post('/api/upload', {
         preHandler: upload.single('file')
     }, async function (request, reply) {
